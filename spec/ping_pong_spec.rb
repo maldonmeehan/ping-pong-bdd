f2cb1ecb7061bef0bc7d18ea5650cf31302103b9
Maldon Meehan and Noah Yasskin <maldon@>
2016-06-27 10:18:49 -0700

    add a second test that passes

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,8 @@ describe('Fixnum#ping_pong?') do
   it("is false for a number not divisible by 3") do
     expect(4.ping_pong?()).to(eq(false))
   end
+
+  it("is true for a number divisible by 3") do
+    expect(3.ping_pong?()).to(eq(true))
+  end
 end
