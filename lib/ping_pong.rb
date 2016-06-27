67342e054bac2554fb5b4428e728d106c37fd543
Maldon Meehan and Noah Yasskin <maldon@>
2016-06-27 10:50:11 -0700

    add three passing tests

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,13 +1,14 @@
 class Fixnum
   define_method(:ping_pong?) do
+    ping_pongs = Array.new
     if self.%(15).eql?(0)
-      true
+      ping_pongs.push("ping-pong")
     elsif self.%(5).eql?(0)
-      true
+      ping_pongs.push("pong")
     elsif self.%(3).eql?(0)
-      true
+      ping_pongs.push("ping")
     else
-      false
+      ping_pongs.push()
     end
   end
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,27 +2,16 @@ require('rspec')
 require('ping_pong')
 
 describe('Fixnum#ping_pong?') do
-  it("is false for a number not divisible by 3") do
-    expect(4.ping_pong?()).to(eq(false))
+
+  it("is ping-pong for a number divisible by 15") do
+    expect(15.ping_pong?()).to(eq(["ping-pong"]))
   end
 
-  it("is true for a number divisible by 3") do
-    expect(3.ping_pong?()).to(eq(true))
+  it("is pong for a number divisible by 5") do
+    expect(5.ping_pong?()).to(eq(["pong"]))
   end
 
-  it("is false for a number not divisible by 5") do
-    expect(7.ping_pong?()).to(eq(false))
-  end
-
-  it("is true for a number divisible by 5") do
-    expect(5.ping_pong?()).to(eq(true))
-  end
-
-  it("is false for a number not divisible by 15") do
-    expect(13.ping_pong?()).to(eq(false))
-  end
-
-  it("is true for a number divisible by 15")do
-    expect(15.ping_pong?()).to(eq(true))
+  it("is ping for a number divisible by 3")do
+    expect(3.ping_pong?()).to(eq(["ping"]))
   end
 end
