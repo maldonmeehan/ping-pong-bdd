1251a5b576cd8a37e7e84f71fcd72f8d2d25c32a
Maldon <maldonmeehan@gmail.com>
2016-07-04 11:17:40 -0700

    fix spacing formate

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,13 +2,17 @@ class Fixnum
   define_method(:ping_pong?) do
     numbers = (1..self)
     ping_pongs = Array.new
+
     numbers.each() do |number|
       if number.%(15).eql?(0)
         ping_pongs.push("ping-pong")
+
       elsif number.%(5).eql?(0)
         ping_pongs.push("pong")
+
       elsif number.%(3).eql?(0)
         ping_pongs.push("ping")
+
       else
         ping_pongs.push(number)
       end
